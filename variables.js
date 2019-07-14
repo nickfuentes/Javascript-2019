f1af03c2845864c283107810d4c036049cd418a7
Full Name <nickfuentes24@gmail.com>
2019-07-14 13:31:45 -0500

    Naming variables and how not to name variables.

---
+++ variables.js
@@ -0,0 +1,12 @@
+// You can not define a variable more than once
+let petName = "Hal"
+petName = "Spot"
+
+console.log(petName)
+
+// There are rulses related to variable names
+let test_ = 2
+let result = 3 + 4
+
+// Variable names cannot be reserved keywords
+// let llet = 12
