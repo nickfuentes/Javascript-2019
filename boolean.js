8c5ebd18bc5a1afb9dd1f2c16d0cc738393c717a
Full Name <nickfuentes24@gmail.com>
2019-07-14 13:59:55 -0500

    Wrote a program with booleans to tell if the age of a person was a child or senior with opertaors.

---
+++ boolean.js
@@ -0,0 +1,26 @@
+// === equality operator
+// !== not equal operator
+// < less than operator
+// > grater than operator
+// <= less than or equal to operator
+// >= greater than or equal to operator
+
+/*
+let temp = "Nickolas"
+let isFreezing = temp <= 32
+
+console.log(isFreezing)
+*/
+
+// Challenge Area
+
+// Create an age variable set to your age
+let age = 65
+// Calculate is child if they are 7 or under
+let child = age <= 7
+// Calculate is senior are 65 or older
+let senior = age >= 66
+// Print is child value 
+console.log(child)
+// Print is senior value
+console.log(senior)
