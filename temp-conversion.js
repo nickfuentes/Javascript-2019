062c54c2bf7762c49f481f7386cee3920a94a378
Full Name <nickfuentes24@gmail.com>
2019-07-14 13:43:29 -0500

    Wrote a program to convert fahrenheit to celsius and kelvin.

---
+++ temp-conversion.js
@@ -0,0 +1,11 @@
+let fahrenheit = 50
+
+// calculate celsius and store in celsius variable
+let celsius = (fahrenheit - 32) * 5 / 9
+//print that value
+console.log(celsius)
+
+//calculate kelvin value and store in variable
+let kelvin = (fahrenheit - 32) * 5 / 9 + 273.15
+// print that value
+console.log(kelvin)
