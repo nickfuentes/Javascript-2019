ea4b1fe33c7b4a7ba51c8283c6fb2965f8036864
Full Name <nickfuentes24@gmail.com>
2019-07-14 13:23:25 -0500

    Wrote a program that calculates a students grade based off the max score and student score and prints the percentage score to the terminal.

---
+++ numbers.js
@@ -0,0 +1,18 @@
+/*
+let age = 26
+let dogYears = (age + 1) / 7
+
+console.log(dogYears)
+*/
+
+// Cahllenege Area
+
+// studentScore 18
+let studentScore = 80
+// maxScore 20
+let maxScore = 100
+// percent 90
+let percent = (studentScore / maxScore) * 100
+
+// print the percent
+console.log(percent)
