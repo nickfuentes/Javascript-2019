7103b4f46a5ee8cc060c38d1b4b1abbdf3e2f38d
Full Name <nickfuentes24@gmail.com>
2019-07-14 13:06:13 -0500

    Practiced with strings and concatinating variables into longer strings stored inside a variable.

---
+++ strings.js
@@ -0,0 +1,13 @@
+/*
+let firstName = "Nickolas"
+let lastName = "Fuentes"
+let fullName = firstName + " " + lastName
+
+console.log(fullName)
+*/
+
+let city = "Houston"
+let country = "United States"
+let location = city + ", " + country
+
+console.log(location)
